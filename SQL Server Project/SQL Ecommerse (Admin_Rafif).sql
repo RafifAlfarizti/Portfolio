c61d4e2143ec2b3ed9b65457b2d9fd8168629f88
Muhammad Rafif Alfarizti <133552004+RafifAlfarizti@users.noreply.github.com>
2024-05-06 14:50:20 +0700

    Delete SQL Server Project directory

--- SQL Server Project/SQL Ecommerse (Admin_Rafif).sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [TransID]
-      ,[ItemID]
-      ,[Quantity]
-  FROM [eCommerse].[dbo].[DetailSell]
--- SQL Server Project/SQL Ecommerse (User_Rafif).sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [ItemName]
-      ,[Price]
-      ,[Quantity]
-  FROM [eCommerse].[dbo].[Item]
